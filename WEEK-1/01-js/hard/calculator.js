16be97e83483ca7994360ede58da222c1b7d8004
Kalash Harode <142109857+kalash-ai@users.noreply.github.com>
2023-12-14 20:28:54 +0530

    calculator.js

--- WEEK-1/01-js/hard/calculator.js
+++ WEEK-1/01-js/hard/calculator.js
@@ -17,6 +17,53 @@
 */
 
 class Calculator {
+
+constructor(result) {
+  this.result = 0;
+}
+add(num) {
+  if (typeof num === "number") {
+    this.result += num;
+  } else {
+    throw new Error("Invalid number");
+  }
+}
+subtract(num) {
+  if (typeof num === "number") {
+    this.result -= num;
+  } else {
+    throw new Error("Invalid number");
+  }
+}
+multiply(num) {
+  if (typeof num === "number") {
+    this.result *= num;
+  } else {
+    throw new Error("Invalid number");
+  }
+}
+divide(num) {
+  if (typeof num === "number" && num !== 0) {
+    this.result /= num;
+  } else {
+    throw new Error("Invalid number");
+  }
+}
+clear() {
+  this.result = 0;
+}
+getResult() {
+  return this.result;
+}
+calculate(expression) {
+  this.result = eval(expression.replace(/\s+/g, " "))
+  if(this.result === Infinity) {
+    throw new Error("Infinity")
+  }
+  return this.result;
+}
+}
+
 //   constructor(){
 //     this.result =0
 //   }
@@ -67,53 +114,6 @@ class Calculator {
 // return this.result
 // }
 
-
-
-constructor(result) {
-  this.result = 0;
-}
-add(num) {
-  if (typeof num === "number") {
-    this.result += num;
-  } else {
-    throw new Error("Invalid number");
-  }
-}
-subtract(num) {
-  if (typeof num === "number") {
-    this.result -= num;
-  } else {
-    throw new Error("Invalid number");
-  }
-}
-multiply(num) {
-  if (typeof num === "number") {
-    this.result *= num;
-  } else {
-    throw new Error("Invalid number");
-  }
-}
-divide(num) {
-  if (typeof num === "number" && num !== 0) {
-    this.result /= num;
-  } else {
-    throw new Error("Invalid number");
-  }
-}
-clear() {
-  this.result = 0;
-}
-getResult() {
-  return this.result;
-}
-calculate(expression) {
-  this.result = eval(expression.replace(/\s+/g, " "))
-  if(this.result === Infinity) {
-    throw new Error("Infinity")
-  }
-  return this.result;
-}
-}
-
-
 module.exports = Calculator;
+
+
